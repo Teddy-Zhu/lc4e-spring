af7bcf5039229511e105fe05ef4b5a5d7ec18f00
Xi Zhu <beyondsoft_ets12@trendmicro.com.cn>
2015-04-16 15:30:28 +0800

    update db sql

--- lc4e.sql
+++ lc4e.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50622
 File Encoding         : 65001
 
-Date: 2015-04-15 17:05:49
+Date: 2015-04-16 15:30:14
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,7 +20,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_areablock`;
 CREATE TABLE `l4_areablock` (
-  `intId` int(11) NOT NULL,
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
   `intBlockedAreaId` int(11) NOT NULL,
   `intUserGroupId` int(11) NOT NULL,
   `dateCreateTime` datetime NOT NULL,
@@ -36,10 +36,9 @@ CREATE TABLE `l4_areablock` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_areaclose`;
 CREATE TABLE `l4_areaclose` (
-  `intId` int(11) NOT NULL,
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
   `intAreaId` int(11) NOT NULL,
-  `intUserId` int(11) NOT NULL,
-  `isClosed` int(11) NOT NULL,
+  `intUserGroupId` int(11) NOT NULL COMMENT 'operate person',
   `dateCreateTime` datetime NOT NULL,
   PRIMARY KEY (`intId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -53,7 +52,7 @@ CREATE TABLE `l4_areaclose` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_areafollow`;
 CREATE TABLE `l4_areafollow` (
-  `intId` int(11) NOT NULL,
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
   `intUserId` int(11) NOT NULL,
   `intFollowedAreaId` int(11) NOT NULL,
   `dateCreateTime` datetime NOT NULL,
@@ -69,11 +68,13 @@ CREATE TABLE `l4_areafollow` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_areas`;
 CREATE TABLE `l4_areas` (
-  `intAreaId` int(11) NOT NULL,
+  `intAreaId` int(11) NOT NULL AUTO_INCREMENT,
   `intParentAreaId` int(11) NOT NULL,
+  `strAreaAbbr` varchar(255) COLLATE utf8_bin NOT NULL,
   `strAreaName` varchar(255) COLLATE utf8_bin NOT NULL,
   `strAreaCss` varchar(255) COLLATE utf8_bin NOT NULL,
   `strAreaIcon` varchar(255) COLLATE utf8_bin NOT NULL,
+  `isShow` int(11) NOT NULL DEFAULT '1',
   `dateCreateTime` datetime NOT NULL,
   `intUserId` int(11) NOT NULL,
   PRIMARY KEY (`intAreaId`)
@@ -88,7 +89,7 @@ CREATE TABLE `l4_areas` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_articleblock`;
 CREATE TABLE `l4_articleblock` (
-  `intId` int(11) NOT NULL,
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
   `intUserId` int(11) NOT NULL,
   `intBlockedArticleId` int(11) NOT NULL,
   `dateCreateTime` datetime NOT NULL,
@@ -104,7 +105,7 @@ CREATE TABLE `l4_articleblock` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_articlefollow`;
 CREATE TABLE `l4_articlefollow` (
-  `intId` int(11) NOT NULL,
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
   `intUserId` int(11) NOT NULL,
   `intFollowedArticleId` int(11) NOT NULL,
   `dateCreateTime` date NOT NULL,
@@ -120,11 +121,13 @@ CREATE TABLE `l4_articlefollow` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_articles`;
 CREATE TABLE `l4_articles` (
-  `intArticleId` int(11) NOT NULL,
+  `intArticleId` int(11) NOT NULL AUTO_INCREMENT,
   `intAreaId` int(11) NOT NULL,
   `intUserId` int(11) NOT NULL,
   `strArticleTitle` varchar(50) COLLATE utf8_bin NOT NULL,
   `strArticleContent` varchar(3000) COLLATE utf8_bin NOT NULL,
+  `isHide` int(11) NOT NULL DEFAULT '0',
+  `isDeleted` int(11) NOT NULL DEFAULT '0',
   `dateCreateTime` datetime NOT NULL,
   `dateModified` datetime NOT NULL,
   PRIMARY KEY (`intArticleId`)
@@ -156,11 +159,13 @@ CREATE TABLE `l4_articletopdown` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_comments`;
 CREATE TABLE `l4_comments` (
-  `intCommentId` int(11) NOT NULL,
+  `intCommentId` int(11) NOT NULL AUTO_INCREMENT,
   `intArticleId` varchar(255) COLLATE utf8_bin NOT NULL,
   `intCommentTitle` varchar(255) COLLATE utf8_bin NOT NULL,
   `intCommentContent` varchar(255) COLLATE utf8_bin NOT NULL,
   `intUserId` int(11) NOT NULL,
+  `isHide` int(11) NOT NULL DEFAULT '0',
+  `isDeleted` int(11) NOT NULL DEFAULT '0',
   `dateCreateTime` datetime NOT NULL,
   `dateModifiedTime` datetime NOT NULL,
   PRIMARY KEY (`intCommentId`)
@@ -175,7 +180,7 @@ CREATE TABLE `l4_comments` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_commenttopdown`;
 CREATE TABLE `l4_commenttopdown` (
-  `intCommentId` int(11) NOT NULL,
+  `intCommentId` int(11) NOT NULL AUTO_INCREMENT,
   `intUserId` int(11) NOT NULL,
   `intTopDown` int(11) NOT NULL,
   `dateCreateTime` datetime NOT NULL,
@@ -191,7 +196,7 @@ CREATE TABLE `l4_commenttopdown` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_locations`;
 CREATE TABLE `l4_locations` (
-  `intLocationId` int(11) NOT NULL,
+  `intLocationId` int(11) NOT NULL AUTO_INCREMENT,
   `intProvinceId` int(11) NOT NULL,
   `intCityId` int(11) NOT NULL,
   `intAreaId` int(11) NOT NULL,
@@ -207,7 +212,7 @@ CREATE TABLE `l4_locations` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_logname`;
 CREATE TABLE `l4_logname` (
-  `intLogNameId` int(11) NOT NULL,
+  `intLogNameId` int(11) NOT NULL AUTO_INCREMENT,
   `strLogNameAbbr` varchar(255) COLLATE utf8_bin NOT NULL,
   `strLogName` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`intLogNameId`)
@@ -222,7 +227,7 @@ CREATE TABLE `l4_logname` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_logs`;
 CREATE TABLE `l4_logs` (
-  `intId` int(11) NOT NULL,
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
   `intLogNameId` int(11) NOT NULL,
   `intUserId` int(11) NOT NULL,
   `dateCreateTime` datetime NOT NULL,
@@ -256,11 +261,11 @@ INSERT INTO `l4_menus` VALUES ('1', '0', '0', '1', 'Menu', 'Menu', 'red', 'brows
 INSERT INTO `l4_menus` VALUES ('2', '1', '1', '5', 'user/signin', 'SignIn', 'basic teal  button', 'user');
 
 -- ----------------------------
--- Table structure for l4_message
+-- Table structure for l4_messages
 -- ----------------------------
-DROP TABLE IF EXISTS `l4_message`;
-CREATE TABLE `l4_message` (
-  `intMessageId` int(11) NOT NULL,
+DROP TABLE IF EXISTS `l4_messages`;
+CREATE TABLE `l4_messages` (
+  `intMessageId` int(11) NOT NULL AUTO_INCREMENT,
   `intUserId` int(11) NOT NULL COMMENT '0 system',
   `intDestUserId` int(11) NOT NULL COMMENT '0 to all',
   `intRead` int(11) NOT NULL COMMENT '0 no read 1 has read',
@@ -272,7 +277,7 @@ CREATE TABLE `l4_message` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
--- Records of l4_message
+-- Records of l4_messages
 -- ----------------------------
 
 -- ----------------------------
@@ -280,13 +285,13 @@ CREATE TABLE `l4_message` (
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_userauthorization`;
 CREATE TABLE `l4_userauthorization` (
-  `intId` int(11) NOT NULL,
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
   `intUserAuthorizationId` int(11) NOT NULL,
   `strUserAuthorizationAbbr` varchar(255) COLLATE utf8_bin NOT NULL,
   `strUserAuthorizationName` varchar(255) COLLATE utf8_bin NOT NULL,
   `intUserAuthorizationValue` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '0',
   PRIMARY KEY (`intId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of l4_userauthorization
@@ -298,31 +303,33 @@ INSERT INTO `l4_userauthorization` VALUES ('1', '1', 'login', 'Login', '0');
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_userbasicinfo`;
 CREATE TABLE `l4_userbasicinfo` (
-  `intUserId` int(11) NOT NULL AUTO_INCREMENT,
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
+  `intUserId` int(11) NOT NULL,
   `strUserName` varchar(255) COLLATE utf8_bin NOT NULL,
   `strUserNick` varchar(255) COLLATE utf8_bin NOT NULL,
   `strEmail` varchar(255) COLLATE utf8_bin NOT NULL,
   `strPassword` varchar(255) COLLATE utf8_bin NOT NULL,
   `strPhoneNumber` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `strSign` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT 'personal sign',
+  `strAvatar` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT 'default.png',
   `intLocationId` int(11) DEFAULT '0',
   `dateBirthDay` datetime DEFAULT NULL,
   `dateCreate` datetime NOT NULL,
   `dateModified` datetime NOT NULL,
-  PRIMARY KEY (`intUserId`)
+  PRIMARY KEY (`intId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of l4_userbasicinfo
 -- ----------------------------
-INSERT INTO `l4_userbasicinfo` VALUES ('1', 'Robot', 'Robot', 'robot@lc4e.com', 'bf1414ea2c125354e4c8f41951637827', '10000', 'lc4e.com@robot!##!.banned', '0', '2015-04-14 15:03:39', '2015-04-14 15:03:43', '2015-04-14 15:03:53');
+INSERT INTO `l4_userbasicinfo` VALUES ('1', '1', 'Robot', 'Robot', 'robot@lc4e.com', 'bf1414ea2c125354e4c8f41951637827', '10000', 'lc4e.com@robot!##!.banned', 'default.png', '0', '2015-04-14 15:03:39', '2015-04-14 15:03:43', '2015-04-14 15:03:53');
 
 -- ----------------------------
 -- Table structure for l4_userblock
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_userblock`;
 CREATE TABLE `l4_userblock` (
-  `intId` int(11) NOT NULL,
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
   `intUserId` int(11) NOT NULL,
   `intBlockedUserId` int(11) NOT NULL,
   `dateCreateTime` datetime NOT NULL,
@@ -376,10 +383,11 @@ INSERT INTO `l4_usergroups` VALUES ('5', 'admin', 'Administrator', '1', '50');
 -- ----------------------------
 DROP TABLE IF EXISTS `l4_usermoney`;
 CREATE TABLE `l4_usermoney` (
+  `intId` int(11) NOT NULL AUTO_INCREMENT,
   `intUserId` int(11) NOT NULL,
   `douMoney` decimal(10,0) NOT NULL,
   `dateModifiedTime` date NOT NULL,
-  PRIMARY KEY (`intUserId`)
+  PRIMARY KEY (`intId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
