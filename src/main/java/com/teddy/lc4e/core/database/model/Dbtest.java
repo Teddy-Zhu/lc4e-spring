684b9c271f4083deb89d6c84edf1b55d6b335b9d
Troevil <304445056@qq.com>
2015-06-26 09:53:23 +0800

    add extend mongodb repository [attach missing files]

---
+++ src/main/java/com/teddy/lc4e/core/database/model/Dbtest.java
@@ -0,0 +1,81 @@
+package com.teddy.lc4e.core.database.model;
+
+import com.teddy.lc4e.core.entity.validate.Str;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by teddy on 2015/6/25.
+ */
+@Document
+public class Dbtest {
+    @Id
+    private ObjectId id;
+
+    @Indexed(unique = true)
+    private String name;
+
+    private String value;
+
+    private String t;
+    private Set<String> tags;
+
+    public Dbtest() {
+    }
+
+    @PersistenceConstructor
+
+    public Dbtest(ObjectId id, String name, String value, String t, Set<String> tags) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+        this.t = t;
+        this.tags = tags;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getT() {
+        return t;
+    }
+
+    public void setT(String t) {
+        this.t = t;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+}
---
+++ src/main/java/com/teddy/lc4e/core/database/repository/DbtestNewRepository.java
@@ -0,0 +1,13 @@
+package com.teddy.lc4e.core.database.repository;
+
+import com.teddy.lc4e.core.database.model.Dbtest;
+import com.teddy.lc4e.core.util.mongodb.BaseMongoRepository;
+import org.bson.types.ObjectId;
+
+/**
+ * Created by teddy on 2015/6/25.
+ */
+public interface DbtestNewRepository extends BaseMongoRepository<Dbtest, ObjectId> {
+
+    Dbtest findByName(String name);
+}
