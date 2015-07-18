687b53528f268eafac0701699ae088a19f2878df
teddy <teddy.zhu@lc4e.com>
2015-07-18 16:24:33 +0800

    prepare for next framework

--- src/main/java/com/teddy/lc4e/plugins/tools/ParserMessage.java
+++ src/main/java/com/teddy/lc4e/plugins/tools/ParserMessage.java
@@ -6,23 +6,23 @@ import org.apache.log4j.Logger;
 import org.springframework.context.MessageSource;
 
 public class ParserMessage {
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = Logger.getLogger(ParserMessage.class);
+    /**
+     * Logger for this class
+     */
+    private static final Logger logger = Logger.getLogger(ParserMessage.class);
 
-	private MessageSource messages;
+    private MessageSource messages;
 
-	public MessageSource getMessages() {
-		return messages;
-	}
+    public MessageSource getMessages() {
+        return messages;
+    }
 
-	public void setMessages(MessageSource messages) {
-		this.messages = messages;
-	}
+    public void setMessages(MessageSource messages) {
+        this.messages = messages;
+    }
 
-	public String L18N(String code, Locale locale, String... args) {
-		String message = messages.getMessage(code, args, code, locale);
-		return message;
-	}
+    public String L18N(String code, Locale locale, String... args) {
+        String message = messages.getMessage(code, args, code, locale);
+        return message == null ? "" : message;
+    }
 }
